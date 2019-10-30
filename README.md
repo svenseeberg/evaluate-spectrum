7b7664c2458c45224c8436015a6471de905bc882
Sven Seeberg <mail@sven-seeberg.de>
2019-10-30 09:13:56 +0100

    Add script and README

---
+++ README.md
@@ -0,0 +1,14 @@
+# What does it do?
+
+This script reads image files provided as an argument. Then calculates
+the vertical average for the red, green, and blue values and writes the
+average and sum of the 3 averages to a CSV file.
+
+# Why does it sum up the RGB values?
+
+This script is about finding absorption lines in spectra. Each color
+value is a signal and we want the best signal-to-noise ratio.
+
+# License
+
+This script is MIT licensed.
---
+++ eval_spectrum.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+ 
+from __future__ import with_statement
+from PIL import Image
+import sys
+ 
+im = Image.open(sys.argv[1]) #relative path to file
+ 
+#load the pixel info
+pix = im.load()
+ 
+#get a tuple of the x and y dimensions of the image
+width, height = im.size
+print(width, height)
+ 
+#open a file to write the pixel data
+with open(sys.argv[1]+'.csv', 'w+') as f:
+  f.write('AVG R,AVG G,AVG B,SUM\n')
+ 
+  #iterate through the x axis
+  for x in range(width):
+    #iterate through the y axis:
+    r = 0
+    b = 0
+    g = 0
+    for y in range(height):
+      r = r + pix[x,y][0]
+      g = g + pix[x,y][1]
+      b = b + pix[x,y][2]
+    r = r / height
+    g = g / height
+    b = b / height
+    print(x,(r,g,b))
+    f.write('{0},{1},{2},{3}\n'.format(r,g,b,sum([r,g,b]))
